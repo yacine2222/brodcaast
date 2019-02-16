4c3313645e9812fc085a8fe2da927a6c98df7bec
yacine2222 <42879964+yacine2222@users.noreply.github.com>
2019-02-16 15:04:29 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,175 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+   client.user.setActivity("Yaciiiin|Music",{type: 'WATCHING'})
+  console.log('')
+  console.log('')
+  console.log('+[-----------------------------------------------------------------]+')
+  console.log(`[Start] ${new Date()}`);
+  console.log('+[-----------------------------------------------------------------]+')
+  console.log('')
+  console.log('+[------------------------------------]+');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('+[------------------------------------]+')
+  console.log('')
+  console.log('+[------------]+')
+  console.log(' Bot Is Online')
+  console.log('+[------------]+')
+  console.log('')
+  console.log('')
+});
+
+var prefix = "b!"
+
+
+
+//bc
+
+client.on("message", message => {
+    if (message.content.startsWith("b!obc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+  message.delete();
+  };
+  });
+
+
+//bc online
+
+
+  var prefix = "b!";
+
+  client.on("message", message => {
+  
+              if (message.content.startsWith(prefix + "bc")) {
+                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(' '); 
+    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+   m.send(`${argresult}\n ${m}`);
+  })
+   message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` :mailbox:  عدد المستلمين `); 
+   message.delete(); 
+  };     
+  });
+
+
+
+
+
+
+client.on('message', message => {
+    var  user = message.mentions.users.first() || message.author;
+if (message.content.startsWith("b!avatar")) {
+message.channel.send(`This avatar For ${user} link : ${user.avatarURL}`);
+}
+});
+
+
+client.on('ready',  () => {
+    console.log('تم تشغيل :Broadcast  ');
+    console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
+  });
+
+
+
+
+
+  client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('b!adminbc')){
+if(!message.author.id === '476185102922285066') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+
+
+  
+  client.on('message', msg => {
+    if(msg.content === 'b!help')
+    msg.reply('Check Your DM :white_check_mark:')
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  client.on("message", message => {
+    if (message.content === "b!help") {
+     const embed = new Discord.RichEmbed() 
+         .setColor("#00FF00")
+         .setThumbnail(message.author.avatarURL)
+         .setDescription(`**Help|هيلب
+       b!obc | لأرسال برود كاست للكل
+       b!bc  |  لأرسال برود كاست للأونلاين
+** `)
+   message.author.sendEmbed(embed)
+   
+   }
+   });
+
+
+ 
+
+ 
+
+
+
+
+
+   client.on('message', message => {
+    if (message.content.startsWith("b!bot")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .setTitle('``INFO Broadcast Bot`` ')
+            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
+            .addField('``servers``', [client.guilds.size], true)
+            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
+            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
+            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
+            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
+                  .addField('``My Prefix``' , `[-]` , true)
+                  .addField('``My Language``' , `[ Java Script ]` , true)
+                    })
+}
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag} !`);
+          client.user.setActivity("b!help",{type: 'WATCHING'});
+  
+  });
+
+
+
+
+client.login(process.env.BOT_TOKEN);
